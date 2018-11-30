ac84d860711cbcb65b6e9074e2ff0cc8d957823c
Narthana Jananjaya <narthanaj@gmail.com>
2018-11-30 09:40:23 +0530

    first push

---
+++ app/build/generated/source/buildConfig/debug/luongvo/com/mycalculator/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package luongvo.com.mycalculator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "luongvo.com.mycalculator";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
